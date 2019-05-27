85651d16bdb1f0a6d5cc5796ed7fc362a01d2d88
geekyTitan <mithilesh.pradhan@outlook.com>
2019-05-27 21:21:50 +0530

    add image classification using mobileNet

--- js/main.js
+++ js/main.js
@@ -1,12 +1,33 @@
+let mobilenet;
+let dog;
+
+function modelReady() {
+  console.log("model ready");
+}
+
+function imageReady() {
+  image(dog, 0, 0, width, height);
+  mobilenet.predict(dog, gotResults);
+}
+
+function gotResults(error, data) {
+  if (error) {
+    console.error(error);
+  } else {
+    console.log(data);
+    let label = data[0].className;
+    let prob = data[0].probability;
+    fill(255);
+    textSize(64);
+    text(label, 10, height - 100);
+    textSize(32);
+    text(prob, 10, height - 50);
+  }
+}
 function setup() {
-    createCanvas(windowWidth, windowHeight);
-}
-
-function draw() {
-    background(255);
-    text("put your p5.js code here",10, frameCount % height);
-}
-
-function windowResized() {
-    resizeCanvas(windowWidth, windowHeight);
+  createCanvas(640, 480);
+  dog = createImg("../images/river.jpg", imageReady);
+  dog.hide();
+  background(0);
+  mobilenet = ml5.imageClassifier("MobileNet", modelReady);
 }
